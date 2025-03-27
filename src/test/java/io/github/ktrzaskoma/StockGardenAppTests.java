ffaf2df4a7eacda2ee1d6718a3ce5a649163b172
ktrzaskoma <kttrzaskoma@gmail.com>
2025-03-27 22:40:32 +0100

    init

---
+++ src/test/java/io/github/ktrzaskoma/StockGardenAppTests.java
@@ -0,0 +1,13 @@
+package io.github.ktrzaskoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockGardenAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
